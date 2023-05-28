b1bf3a73290c9fd6a276034e2696b2cc15084199
mim-mel <mfblade2014@gmail.com>
2023-05-28 21:05:41 +0900

    feat: 서버 폴더 초기세팅

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+//app.get url로 접속하면 해당 블록의 코드를 실행
+app.get('/', (_, res) => res.send('running'));
+
+let port = 4000;
+
+//app.listen의 포트로 접속하면 해당 블록의 코드를 실행
+app.listen(port, async () => {
+  console.log(`server running at http://localhost:${port}`);
+});
