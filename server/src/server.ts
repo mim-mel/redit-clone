893de01df6074cc782d9bd8be1822d30549f92b2
mim_mel <mfblade2014@gmail.com>
2023-05-31 12:03:35 +0900

    feat: 데이터베이스 연결

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
@@ -12,6 +13,13 @@ app.get('/', (_, res) => res.send('running'));
 let port = 4000;
 
 //app.listen의 포트로 접속하면 해당 블록의 코드를 실행
-app.listen(port, async () => {
+app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
+
+  //서버가 실행이 되면 데이터베이스를 connection해줌
+  AppDataSource.initialize()
+    .then(async () => {
+      console.log('database initialized');
+    })
+    .catch(error => console.log(error));
 });
