8233da2d042fa9dea78a6d9c9471e7e429974db6
mim_mel <mfblade2014@gmail.com>
2023-05-31 16:14:32 +0900

    feat: User Entity 생성

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,18 +1,39 @@
-// import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsEmail, Length } from 'class-validator';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Index,
+  OneToMany,
+  BeforeInsert,
+} from 'typeorm';
+import bcrypt from 'bcryptjs';
 
-// @Entity()
-// export class User {
+@Entity('users')
+export class User {
+  @Index()
+  @IsEmail(undefined, { message: '이메일 형식으로 입력해 주세요' })
+  @Length(1, 255, { message: '이메일 주소는 비워둘 수 없습니다' })
+  @Column({ unique: true })
+  email: string;
 
-//     @PrimaryGeneratedColumn()
-//     id: number
+  @Index()
+  @Length(3, 32, { message: '사용자 이름은 3자 이상 입력해주세요' })
+  @Column()
+  username: string;
 
-//     @Column()
-//     firstName: string
+  @Column()
+  @Length(6, 255, { message: '비밀번호를 6자 이상 입력해주세요' })
+  password: string;
 
-//     @Column()
-//     lastName: string
+  @OneToMany(() => Post, post => post.user)
+  posts: Post[];
 
-//     @Column()
-//     age: number
+  @OneToMany(() => Vote, vote => vote.user)
+  votes: Vote[];
 
-// }
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+}
