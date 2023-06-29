b8687827b3de0ad88cd17693b344adeddf288a29
mim_mel <mfblade2014@gmail.com>
2023-06-29 16:57:37 +0900

    feat: next 프로젝트에 tailwind 적용하기

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
